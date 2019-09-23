437f06850e385251454690e856b71a8be4987d1e
daclouds <daclouds@gmail.com>
2019-09-23 10:40:40 +0900

    Modify columns

--- main.py
+++ main.py
@@ -17,13 +17,17 @@ def root():
 
 def analysis(params):
   result = json.dumps(params, ensure_ascii=False)
-  result = eval(result)
+  r = eval(result)
 
-  data = pd.read_csv('./data.csv')
-  data_X = data.drop(['person_id','death'],axis=1)
-  data_Y = data['death']
-  from sklearn.linear_model import LogisticRegression
-  lr = LogisticRegression(C=10000)
-  lr.fit(data_X, data_Y)
-  pred_y = lr.predict(data_X)
-  return {'normal': [], 'result': pred_y[0]}
+  from math import exp
+  X =-2.456741+0.4386*int(r['gender'])-0.114148*int(r['under_gfr_min'])+0.3673*int(r['under_cr_max'])+0.604602*int(r['alt_max'])+0.069*int(r['ast_max'])+0.32*int(r['a1c_max'])+0.44*int(r['alp_max'])+0.47*int(r['bil_max'])+0.097*int(r['d1_cnt'])-0.28*int(r['hbv'])+0.015*int(r['age'])
+  return round(exp(X)/(1+exp(X)), 3) * 100
+  
+  # data = pd.read_csv('./data.csv')
+  # data_X = data.drop(['person_id','death'],axis=1)
+  # data_Y = data['death']
+  # from sklearn.linear_model import LogisticRegression
+  # lr = LogisticRegression(C=10000)
+  # lr.fit(data_X, data_Y)
+  # pred_y = lr.predict(data_X)
+  # return {'normal': [], 'result': pred_y[0]}
--- static/javascripts/app.js
+++ static/javascripts/app.js
@@ -1,36 +1,53 @@
-var columns = 'name,strength,intelligence,charisma,dexterity,luck\n';
-
+var columns = 'name,gender,under_gfr_min,under_cr_max,alt_max,ast_max,a1c_max,alp_max,bil_max,d1_cnt,hbv,age\n'
+  + 'Default,1,0,1,1,1,1,1,1,0.58,0,0.52\n';
 function showRadar(){
-  var nameInput = document.getElementById("name");
-  var strengthInput = document.getElementById("strength");
-  var intelligenceInput = document.getElementById("intelligence");
-  var charismaInput = document.getElementById("charisma");
-  var dexterityInput = document.getElementById("dexterity");
-  var luckInput = document.getElementById("luck");
+  var gender = document.getElementById("gender");
+  var under_gfr_min = document.getElementById("under_gfr_min");
+  var under_cr_max = document.getElementById("under_cr_max");
+  var alt = document.getElementById("alt_max");
+  var ast = document.getElementById("ast_max");
+  var a1c = document.getElementById("a1c_max");
+  var alp = document.getElementById("alp_max");
+  var bil = document.getElementById("bil_max");
+  var d1 = document.getElementById("d1_cnt");
+  var hbv = document.getElementById("hbv");
+  var age = document.getElementById("age");
 
   var wInput = document.getElementById("w");
   var hInput = document.getElementById("h");
-  // var dataInput = document.getElementById("data");
 
+  // var dataInput = document.getElementById("data");
   // var c = document.getElementById("data").value,
+
   c = columns + [
-    nameInput.value,
-    strengthInput.value,
-    intelligenceInput.value,
-    charismaInput.value,
-    dexterityInput.value,
-    luckInput.value,
+    gender.value,
+    under_gfr_min.value,
+    under_cr_max.value,
+    alt.value,
+    ast.value,
+    a1c.value,
+    alp.value,
+    bil.value,
+    d1.value,
+    hbv.value,
+    age.value,
   ].join(",");
   const params = {
-    name: nameInput.value,
-    strength: strengthInput.value,
-    intelligence: intelligenceInput.value,
-    charisma: charismaInput.value,
-    dexterity: dexterityInput.value,
-    luck: luckInput.value,
+    gender: gender.value,
+    under_gfr_min: under_gfr_min.value,
+    under_cr_max: under_cr_max.value,
+    alt_max: alt.value,
+    ast_max: ast.value,
+    a1c_max: a1c.value,
+    alp_max: alp.value,
+    bil_max: bil.value,
+    d1_cnt: d1.value,
+    hbv: hbv.value,
+    age: age.value,
   };
   axios.post('/hello', params).then(function (response) {
     console.log(response);
+    document.getElementById("result").innerHTML = 'Failure: ' + Math.round(response.data) + '%';
     drawGraph(_.values(_.map(params, (n) => isNaN(n) ? n : parseInt(n, 10))));
   });
 }
@@ -43,7 +60,7 @@ function drawGraph(response) {
   w = document.getElementById("w").value;
   h = document.getElementById("h").value;
 
-  var c = columns + _.values(response).join(",");
+  var c = columns + 'User,' + _.values(response).join(",");
 
   csv = c.split("\n").map(function(i){return i.split(",")})
   headers = []
@@ -57,7 +74,20 @@ function drawGraph(response) {
           newSeries.className = v;
           newSeries.axes = [];
         }else{
-          newSeries.axes.push({"axis":[headers[j]], "value": parseFloat(v)});
+          let value;
+          switch (j) {
+            case 9:
+              value = v / 6;
+              console.log('d1_cnt', value)
+              break;
+            case 11:
+              value = v /98;
+              console.log('age', value)
+              break;
+            default:
+              value = v;
+          }
+          newSeries.axes.push({"axis":[headers[j]], "value": parseFloat(value)});
         }
       });
       data.push(newSeries);
